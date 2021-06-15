1798f1889d25b64e0db0e5f93bd37941fcdef7df
narh <kpododanielnarh@gmail.com>
2021-06-15 17:12:38 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello World!",
+    };
+  }
+  handeleButton = (e) => {
+    this.setState({
+      string: "Hallelujah",
+    });
+  };
+  render() {
+    return (
+      <div className="app">
+        <h2>The Lord is my {this.state.string}</h2>
+        <button onClick={this.handeleButton}>Chane Text</button>
+      </div>
+    );
+  }
 }
 
 export default App;
