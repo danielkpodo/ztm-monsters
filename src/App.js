2c5582ce96cfff7b2183c49b605315bae8015e46
narh <kpododanielnarh@gmail.com>
2021-06-16 05:53:04 +0000

    retrieved data from json placeholder

--- src/App.js
+++ src/App.js
@@ -7,24 +7,52 @@ const monsters = [...Array(10)].map((monster) => {
     id: faker.datatype.uuid(),
     name: faker.name.findName(),
     email: faker.internet.email(),
+    image: faker.image.imageUrl(),
   };
 });
 
 class App extends Component {
-  state = {
-    monsters,
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    handleDataRequest("https://jsonplaceholder.typicode.com/users")
+      .then((users) => {
+        this.setState({ monsters: users, isLoading: true });
+      })
+      .catch((err) => {
+        throw new Error("Can't return data from url");
+      });
+  }
+
+  dynamicContent = () => {
+    if (this.state.isLoading) {
+      return this.state.monsters.map((monster) => {
+        return <li key={monster.id}>{monster.name}</li>;
+      });
+    } else {
+      return <p>Lodaing...</p>;
+    }
   };
 
   render() {
     console.log(this.state.monsters);
-    return (
-      <div className="app">
-        {this.state.monsters.map((monster) => {
-          return <li key={monster.id}>{monster.id}</li>;
-        })}
-      </div>
-    );
+    return <div className="app">{this.dynamicContent()}</div>;
   }
 }
 
 export default App;
+
+const handleDataRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+  });
+};
